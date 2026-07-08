53fee87da793638d9f442660a2df212908edf928
Hemalakshmi0327 <monisha.lathi@gmail.com>
2026-07-08 13:12:36 +0530

    Add files via upload

---
+++ Day1assignment/Datatypes-let.js
@@ -0,0 +1,31 @@
+//String
+
+let userName = "Hema"
+console.log(userName);
+console.log(typeof userName);
+
+//string
+
+let organization = "BNPP"
+console.log(organization);
+console.log(typeof organization );
+
+//Number
+let contactNumber = 12345
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+//Boolean
+let knowsAutomation = true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+//undefined
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
+
---
+++ Day1assignment/Day1-Datatype.js
@@ -0,0 +1,31 @@
+//String
+
+var firstName = "Hema"
+console.log(firstName);
+console.log(typeof firstName);
+
+//string
+
+var companyName = "BNPP"
+console.log(companyName);
+console.log(typeof companyName );
+
+//Number
+var mobileNumber = 12345
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+//Boolean
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//undefined
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
