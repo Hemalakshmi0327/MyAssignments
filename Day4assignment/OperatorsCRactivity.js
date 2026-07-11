047d8577de2c0e8d3e86225a3224ecc2d2e2f3ae
Hemalakshmi0327 <monisha.lathi@gmail.com>
2026-07-11 13:04:04 +0530

    Add files via upload

---
+++ Day4assignment/OperatorsCRactivity.js
@@ -0,0 +1,8 @@
+let i =0
+function printOddNumbers(){
+    for (i=0; i<=25; i++)
+        if (i % 2 != 0) 
+            console.log(i);
+        
+}
+printOddNumbers()
---
+++ Day4assignment/squareinreverse.js
@@ -0,0 +1,5 @@
+//Squares in Reverse Order//
+
+for (let i=10; i>=1 ; i--) {
+    console.log("The square of " + i + " is " + (i*i));
+}
